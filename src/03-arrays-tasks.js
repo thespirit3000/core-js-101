35cc49974b4d4037064fc2eb9631df47de60bce3
Dzianis Bogdan <thespirit3000@gmail.com>
2019-12-22 20:42:09 +0300

    feat: array completion

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -20,8 +20,8 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
@@ -35,8 +35,11 @@ function findElement(/* arr, value */) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  let arr = Array.from(Array(len * 2), (x, index) => index + 1);
+  arr = arr.filter((x) => x % 2 !== 0);
+  arr.length = len;
+  return arr;
 }
 
 
